2943bd20313814e432fe06742e50bd0ba5a157cf
Leo Gunadarma <67279463+Leo-gunadarma@users.noreply.github.com>
2021-05-07 12:11:56 +0800

    Updat SQL Dump nya
    SQL terbaru untuk soft dlete dan modul 3

--- db_prak_prognet.sql
+++ db_prak_prognet.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Ultimate v12.5.1 (64 bit)
-MySQL - 10.1.35-MariaDB : Database - db_prak_prognet
+SQLyog Ultimate v13.1.1 (64 bit)
+MySQL - 10.4.17-MariaDB : Database - db_prak_prognet
 *********************************************************************
 */
 
@@ -12,6 +12,10 @@ MySQL - 10.1.35-MariaDB : Database - db_prak_prognet
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_prak_prognet` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `db_prak_prognet`;
+
 /*Table structure for table `admin_notifications` */
 
 DROP TABLE IF EXISTS `admin_notifications`;
@@ -87,7 +91,7 @@ CREATE TABLE `couriers` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `couriers` */
 
@@ -110,6 +114,22 @@ CREATE TABLE `discounts` (
 
 /*Data for the table `discounts` */
 
+/*Table structure for table `failed_jobs` */
+
+DROP TABLE IF EXISTS `failed_jobs`;
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `failed_jobs` */
+
 /*Table structure for table `migrations` */
 
 DROP TABLE IF EXISTS `migrations`;
@@ -119,7 +139,7 @@ CREATE TABLE `migrations` (
   `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `migrations` */
 
@@ -142,7 +162,9 @@ insert  into `migrations`(`id`,`migration`,`batch`) values
 (16,'2019_02_16_064643_create_couriers_table',1),
 (17,'2019_02_16_102028_create_notifications_table',1),
 (18,'2019_02_16_103007_create_seller_notifications_table',1),
-(19,'2019_02_16_103024_create_user_notifications_table',1);
+(19,'2019_02_16_103024_create_user_notifications_table',1),
+(20,'2019_08_19_000000_create_failed_jobs_table',2),
+(21,'2021_04_19_095655_insert_column_deleted_at_to_table_product',2);
 
 /*Table structure for table `password_resets` */
 
@@ -167,9 +189,18 @@ CREATE TABLE `product_categories` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `product_categories` */
+
+insert  into `product_categories`(`id`,`category_name`,`created_at`,`updated_at`) values 
+(10,'Android','2021-03-29 02:36:47','2021-03-29 02:36:47'),
+(11,'IOS','2021-04-15 16:53:18','2021-04-15 16:53:18'),
+(12,'4G','2021-04-15 16:54:42','2021-04-15 16:54:42'),
+(13,'5G','2021-04-15 16:57:36','2021-04-15 16:57:36'),
+(14,'UI','2021-04-15 16:58:54','2021-04-15 16:58:54'),
+(15,'Tablet','2021-04-15 16:59:39','2021-04-15 16:59:39'),
+(16,'Gaming','2021-04-15 16:59:55','2021-04-15 16:59:55');
 
 /*Table structure for table `product_category_details` */
 
@@ -179,16 +210,29 @@ CREATE TABLE `product_category_details` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `product_id` int(10) unsigned NOT NULL,
   `category_id` int(10) unsigned NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
   KEY `product_id` (`product_id`),
   KEY `category_id` (`category_id`),
   CONSTRAINT `product_category_details_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
   CONSTRAINT `product_category_details_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `product_categories` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 
 /*Data for the table `product_category_details` */
+
+insert  into `product_category_details`(`id`,`product_id`,`category_id`,`created_at`,`updated_at`) values 
+(16,29,10,'2021-04-19 08:57:28','2021-04-19 08:57:28'),
+(18,31,11,'2021-04-23 12:08:07','2021-04-23 12:08:07'),
+(19,31,12,'2021-04-23 12:08:07','2021-04-23 12:08:07'),
+(20,28,10,'2021-04-24 13:22:54','2021-04-24 13:22:54'),
+(21,28,11,'2021-04-24 13:22:54','2021-04-24 13:22:54'),
+(22,28,12,'2021-04-24 13:22:54','2021-04-24 13:22:54'),
+(23,28,13,'2021-04-24 13:22:54','2021-04-24 13:22:54'),
+(24,28,14,'2021-04-24 13:22:54','2021-04-24 13:22:54'),
+(25,28,16,'2021-04-24 13:22:54','2021-04-24 13:22:54'),
+(26,30,14,'2021-04-24 13:23:04','2021-04-24 13:23:04'),
+(27,30,15,'2021-04-24 13:23:04','2021-04-24 13:23:04');
 
 /*Table structure for table `product_images` */
 
@@ -203,9 +247,18 @@ CREATE TABLE `product_images` (
   PRIMARY KEY (`id`),
   KEY `product_id` (`product_id`),
   CONSTRAINT `product_images_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `product_images` */
+
+insert  into `product_images`(`id`,`product_id`,`image_name`,`created_at`,`updated_at`) values 
+(22,28,'samsung A12(3).jpg','2021-04-16 06:06:33','2021-04-16 14:08:56'),
+(23,28,'oppoA3S.jpg','2021-04-16 06:06:33','2021-04-16 13:51:35'),
+(24,29,'landscape walpaper..jpg','2021-04-19 08:57:28','2021-04-19 08:57:28'),
+(25,30,'a8.jpeg','2021-04-19 15:05:31','2021-04-19 15:05:31'),
+(26,31,'oppoA3S(2).jpg','2021-04-23 12:08:06','2021-04-23 12:08:06'),
+(27,31,'oppoA3S(3).jpg','2021-04-23 12:08:06','2021-04-23 12:08:06'),
+(28,31,'6082b8a70e2e2-oppoA3S.jpg','2021-04-23 12:08:07','2021-04-23 12:08:07');
 
 /*Table structure for table `product_reviews` */
 
@@ -241,12 +294,19 @@ CREATE TABLE `products` (
   `product_rate` double DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
   `stock` int(10) DEFAULT NULL,
   `weight` int(3) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `products` */
+
+insert  into `products`(`id`,`product_name`,`price`,`description`,`product_rate`,`created_at`,`updated_at`,`deleted_at`,`stock`,`weight`) values 
+(28,'Xiaomi Mi Note 10',6000000,'Xiaomi Mi Note 10 memiliki diagonal layar 6,47 inci dengan resolusi 2.340 x 1.080 piksel. Di bagian atasnya terdapat poni kecil model waterdrop, yang memuat kamera selfie beresolusi 32 MP. Layar Xiaomi Mi Note 10 sudah dilapisi teknologi Gorilla Glass 5',NULL,'2021-04-16 06:06:33','2021-04-24 13:22:54',NULL,10,208),
+(29,'Sony Experia Z1',7000000,'Sony Experia Z1 adalah sebuah handphone android keluaran Sony',NULL,'2021-04-19 08:57:28','2021-04-19 08:57:28',NULL,10,5),
+(30,'Samsung A8+',2000000,'Samsung A8 plus adalah produk handphone yang dikeluarkan oleh Samsung.',NULL,'2021-04-19 15:05:31','2021-04-24 13:23:04',NULL,8,20),
+(31,'Oppo A3S',2555000,'Hp Opppo',NULL,'2021-04-23 12:08:06','2021-04-23 12:08:06','2021-04-23 12:12:45',25,168);
 
 /*Table structure for table `response` */
 
@@ -257,7 +317,7 @@ CREATE TABLE `response` (
   `review_id` int(10) unsigned NOT NULL,
   `admin_id` int(10) unsigned NOT NULL,
   `content` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
   KEY `review_id` (`review_id`),
@@ -356,12 +416,14 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `users` */
 
 insert  into `users`(`id`,`name`,`email`,`profile_image`,`status`,`email_verified_at`,`password`,`remember_token`,`created_at`,`updated_at`) values 
-(3,'illyasviel Von Einzbern','pandupratama963@gmail.com','-','0','2021-03-07 10:01:57','$2y$10$xKt0EBmrMC6SVDb.flzZUuDhNOII5K1vu/NSR.0g7dUZT4odNmLCe','8noFNHIxKuWuaBY8u6uW1LINcSHSc3dL5e5jo68TRdXswFwGYC1MNfg4ywUr','2021-03-07 10:00:29','2021-03-07 10:01:57');
+(3,'illyasviel Von Einzbern','pandupratama963@gmail.com','-','0','2021-03-07 10:01:57','$2y$10$xKt0EBmrMC6SVDb.flzZUuDhNOII5K1vu/NSR.0g7dUZT4odNmLCe','8noFNHIxKuWuaBY8u6uW1LINcSHSc3dL5e5jo68TRdXswFwGYC1MNfg4ywUr','2021-03-07 10:00:29','2021-03-07 10:01:57'),
+(4,'Kadek Frama','dframa19@gmail.com','-','0',NULL,'$2y$10$1Yxbb2qs1OUN8LQuuWfcoOk.o60rz7xLj3hb5LP1NLMPxkMMavkj.',NULL,'2021-04-18 01:04:55','2021-04-18 01:04:55'),
+(5,'User Kelompok 8','prognet2021@gmail.com','-','0','2021-04-23 09:38:34','$2y$10$qUeeV4tLKphg0jP7R/MDuO/65mW7N279IZVKmrYQZDVHeXP5UWUrO',NULL,'2021-04-23 09:38:15','2021-04-23 09:38:34');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
